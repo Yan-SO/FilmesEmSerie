1d235c8a20d009201394872ef59a13075b024dd5
unknown <yanoliveira2001@gmail.com>
2023-04-26 15:53:23 -0300

    todas as telas

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import Cadastro from './src/screens/cadastro';
+import Home from './src/screens/home';
+import Lista from './src/screens/lista';
+import AddConteudo from './src/screens/addConteudo';
+
+
+export default function App() {
+  return (
+    <AddConteudo />
+  );
+}
+
